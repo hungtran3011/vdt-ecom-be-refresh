fd99bcd663a3ca3a7ae0740ecc0ccba7ac184c92
hungtran3011 <hungtran30112004@gmail.com>
2025-05-27 10:47:42 +0700

    init commit

---
+++ src/main/java/com/hungng3011/vdtecomberefresh/VdtEcomBeRefreshApplication.java
@@ -0,0 +1,13 @@
+package com.hungng3011.vdtecomberefresh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VdtEcomBeRefreshApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VdtEcomBeRefreshApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hungng3011/vdtecomberefresh/VdtEcomBeRefreshApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hungng3011.vdtecomberefresh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VdtEcomBeRefreshApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
